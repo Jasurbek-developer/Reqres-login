810dbccc9fcf1845a71f0ab6b7cf90a3671bb934
Jasurbek <117120741+Jasurbek-developer@users.noreply.github.com>
2023-04-03 10:30:04 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+fetch("https://reqres.in/api/users")
+.then(response=>response.json())
+.then(json=>{
+    console.log(json.data);
+    const markup=json.data.map(el=>{
+        return `
+        <li class="card-container">
+            <div class="img-container">
+                <img class="round" src="${el.avatar}">
+            </div>
+            <div class="name-container">
+                <span class="firstName">${el.first_name}</span>
+                <span class="lastName">${el.last_name}</span>
+            </div>
+                <p class="email">${el.email}</p>
+        </li>
+    `
+    })
+    document.querySelector(".list-container").innerHTML=markup
+    
+})
